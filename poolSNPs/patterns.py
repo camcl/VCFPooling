GL computation corrected
Big combinatorial data sets from generators.
Computationally expensive (several hours).
Used itertools for implementing iterators/generators instead of array-structures. + multiprocessing for relieving memory.

poolSNPs/patterns.py
@@ -1,9 +1,10 @@
-from cyvcf2 import VCF
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.stats import *
 import itertools
+import multiprocessing as mp
+from collections import Counter
+
 from scripts.poolSNPs import parameters as prm
 from scripts.poolSNPs import pool
 from scripts.poolSNPs.alleles import alleles_tools as alltls
@@ -12,28 +13,53 @@ from scripts.poolSNPs import results as res
 from persotools.debugging import *
 from persotools.files import *
 
+import inspect
+import time
 
-def combine_gl(nbSlots, nbIdv, gtpos=['RA', 'AA']):
+
+def report(xk):
+    frame = inspect.currentframe().f_back
+    #print(frame)
+    locals = frame.f_locals
+    #print(locals)
+    return locals
+
+
+def combine_gt(nbIdv, gtpos=['RA', 'AA']):
     prd = itertools.combinations_with_replacement(gtpos, r=nbIdv)
-    patterns = list()
-    for p in prd:
-        tpl = p + tuple(['RR'] * (nbSlots - nbIdv))
-        patterns.append(tpl)
-    pot = np.array(patterns).flatten()
-    # adjusted GLs
-    wgl = np.array([np.count_nonzero(np.where(pot == let, 1, 0)) for let in ['RR', 'RA', 'AA']]) / len(pot)
-    return wgl.squeeze()
+    return prd
+
+
+def permute_gt(nbSlots, p):
+    tpl = p + tuple(['RR'] * (nbSlots - len(p)))
+    t = itertools.permutations(tpl)
+    iter_set = set(s for s in t).__iter__()
+    return iter_set
+
+
+def parallel_permut(nbSlots, prd):
+    ptask = mp.Pool(processes=4)
+    args = tuple(zip(itertools.repeat(nbSlots),
+                     [p for p in prd]))
+    results = ptask.starmap(permute_gt, args)
+    ptask.close()
+    ptask.join()
+    patterns = iter(r for r in results)
+    return patterns
 
 
 def generate_patterns(nbRow, nbCol, nbIdv):
-    n = max((nbRow, nbCol))
-    N = nbRow * nbCol
+    combi = combine_gt(nbIdv)
+    patterns = parallel_permut(nbRow*nbCol, combi)
     patset = list()
-    comb = itertools.combinations(range(N), r=nbIdv)
-    for cb in comb:
-        p = np.isin(np.arange(0, N), cb).reshape((nbRow, nbCol))
-        patset.append(p.astype(int))
-    return patset
+    t = np.vectorize(lambda x: 0 if x == 'RR' else 1)
+    for p in patterns:
+        for a in p:
+            q = np.apply_along_axis(t,
+                                    axis=0,
+                                    arr=a)
+            patset.append((a, q))
+    return patset.__iter__()
 
 
 def count_pos(arr): # counts carriers if on raw samples
@@ -63,19 +89,18 @@ def count_pattern(arr): # counts carriers if on raw samples
 
 
 def constraint_pattern(nbRow, nbCol, nbIdv):
-    patset = generate_patterns(nbRow, nbCol, nbIdv)
-    res = 0
-    for p in patset:
-        # print(p)
-        r, c = count_pattern(p)
-        if r * c < np.size(p):
+    # filters patterns
+    patin = generate_patterns(nbRow, nbCol, nbIdv)
+    patout = list()
+    for p in patin:
+        r, c = count_pattern(p[1].reshape((nbRow, nbCol)))
+        if r * c < np.size(p[1]):
             continue
             # print('Reject')
         else:
             #print('Accept')
-            res += 1
-        # print('')
-    return res
+            patout.append(p[0])
+    return itertools.chain.from_iterable(patout)
 
 
 def screen_patterns(nbRow, nbCol):
@@ -93,14 +118,15 @@ def weight_gl(nbRow, nbCol):
     else:
         n = max((nbRow, nbCol))
         N = nbRow * nbCol
-        gl = np.zeros(3)
-        den = 0
-        for i in range(n, N + 1, 1):
-            tot = constraint_pattern(nbRow, nbCol, i)
-            ratios = combine_gl(N, i)
-            gl = np.sum(np.vstack([gl, tot * ratios]), axis=0)
-            den += tot
-        w = gl / den
+        pot = itertools.starmap(constraint_pattern,
+                                zip(itertools.repeat(nbRow, times=N-n+1),
+                                    itertools.repeat(nbCol, times=N-n+1),
+                                    range(n, N+1))
+                                )
+        cnt = Counter(itertools.chain.from_iterable(pot))
+        ratios = np.array([cnt['RR'], cnt['RA'], cnt['AA']])
+        w = ratios / sum(([cnt['RR'], cnt['RA'], cnt['AA']]))
+        print('Processed: ({}, {}) {}'.format(nbRow, nbCol, w))
     return w
 
 
@@ -109,7 +135,11 @@ def enumerate_comb():
     weighted = {('0', '0'): np.array([1/3, 1/3, 1/3])}
     for c in cb:
         weighted[(str(c[0]), str(c[1]))] = weight_gl(c[0], c[1])
-    return weighted
+        # save in a file!
+    df = pd.DataFrame.from_dict(data=list([k[0], k[1], i] for k,i in weighted.items()),
+                                orient='index',
+                                columns=['n', 'm', 'gl'])
+    df.to_csv(os.path.join(prm.WD, 'adaptative_gl.csv'))
 
 
 def decoding_power(mtx=pool.SNPsPool().design_matrix()):
@@ -153,39 +183,22 @@ def replace_gl(variant, d):
     return g_out
 
 
-def adaptative_likelihood_converter(f_in, f_out):
-    d = enumerate_comb()
+def adaptative_likelihood_converter(f_in, f_out, write=False):
+    if write:
+        # write gl and combinations
+        enumerate_comb()
+    df = pd.read_csv(os.path.join(prm.WD, 'adaptative_gl.csv'))
+    d = df.to_dict()
     replace = lambda v: replace_gl(v, d)
     alltls.file_likelihood_converter(f_in,
                                      f_out,
                                      func=replace)
 
 
-# w22 = weight_gl(2, 2)
-# print('2x2\r')
-# print(w22)
-# print('\r\n')
-# w23 = weight_gl(2, 3)
-# print('2x3\r')
-# print(w23)
-# print('\r\n')
-# w33 = weight_gl(3, 3)
-# print('3x3\r')
-# print(w33)
-# print('\r\n')
-# w34 = weight_gl(3, 4)
-# print('3x4\r')
-# print(w34)
-# print('\r\n')
-# w44 = weight_gl(4, 4)
-# print('4x4\r')
-# print(w44)
+# if __name__ == '__main__':
+os.chdir(prm.WD)
 
-if __name__ == '__main__':
-    os.chdir(prm.WD)
-    d = enumerate_comb()
-    print(d)
-    replace = lambda v: replace_gl(v, d)
-    alltls.file_likelihood_converter('IMP.chr20.pooled.snps.gt.chunk1000.vcf.gz',
-                                     'IMP.chr20.pooled.snps.adaptative_gl.chunk1000.vcf',
-                                     func=replace)
+start = time.time()
+enumerate_comb()
+stop = time.time()
+print('Elapsed time: ', stop-start)