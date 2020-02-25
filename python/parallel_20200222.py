a390235f0e14c1b83b465ddb076337f071e739bf
camcl <camille.clouard@it.uu.se>
2020-02-25 16:21:38 +0100

    Refactor withm independant paths

--- python/parallel_20200222.py
+++ python/parallel_20200222.py
@@ -31,7 +31,7 @@ Steps:
 * Merge pooled chunks back to read-for-use compressed VCF file
 
 Usage: 
-$ python3 parallel_20200222.py /home/camille/1000Genomes/data/gt/ALL.chr20.snps.gt.chunk10000.vcf.gz /home/camille/1000Genomes/data/gl/gl_adaptive/parallel_pooling/ALL.chr20.pooled.snps.gl.chunk10000.vcf.gz 2
+$ python3 parallel_20200222.py ~/1000Genomes/data/gt/ALL.chr20.snps.gt.chunk10000.vcf.gz ~/1000Genomes/data/gl/gl_adaptive/parallel_pooling/ALL.chr20.pooled.snps.gl.chunk10000.vcf.gz 2
 '''
 
 ### COMMAND-LINE PARSING AND PARAMETERS
@@ -44,6 +44,10 @@ parser.add_argument('cores', metavar='nc', type=int, nargs='?', help='Number of 
 argsin = parser.parse_args()
 
 nb_cores = os.cpu_count() if argsin.cores is None else argsin.cores
+try:
+    tmp_pathh = os.environ['SNIC_TMP']
+except EnvironmentError:
+    tmp_path = prm.TMP_DATA_PATH
 
 print('\n'.ljust(80, '*'))
 print('Number of cpu to be used = {}'.format(nb_cores))
@@ -51,27 +55,8 @@ print('Input file = {}'.format(argsin.pathin))
 print('Output file = {}'.format(argsin.pathout))
 print('\n'.rjust(80, '*'))
 
-# chk_sz = prm.CHK_SZ
-# path_gt_files = prm.PATH_GT_FILES
-# path_gl_files = prm.PATH_GL_FILES
-#
-# if prm.GTGL == 'GT':
-#     cd = path_gt_files
-# if prm.GTGL == 'GL':
-#     if prm.unknown_gl != 'adaptive':
-#         cd = os.path.join(path_gl_files, 'gl_' + '_'.join(np.around(prm.unknown_gl, decimals=2).astype(str)))
-#     else:
-#         cd = os.path.join(path_gl_files, 'gl_adaptive')
-#     mkdir(cd)
-# os.chdir(cd)
-#
-# raw = NamedDict('raw', list(prm.RAW.keys()), list(prm.RAW.values()))
-# pooled = NamedDict('pooled', list(prm.POOLED.keys()), list(prm.POOLED.values()))
-#
-# print('\nStart processing files for running BEAGLE'.ljust(80, '.'))
-
 ### SPLIT MAIN VCF-FILE INTO PACKS
-os.chdir(prm.TMP_DATA_PATH)
+os.chdir(tmp_pathh)
 fingz = argsin.pathin  # '/home/camille/1000Genomes/data/gt/ALL.chr20.snps.gt.chunk10000.vcf.gz'
 basin = os.path.basename(fingz).rstrip('.gz')
 basout = os.path.basename(argsin.pathout)
@@ -97,7 +82,7 @@ cyvcfpack = cyvcfchunk.chunkpacker()  # generator of generators
 start = time.time()
 prename = 'pack'
 indices = np.arange(len([*cyvcfpack]))
-files0 = [os.path.join(prm.TMP_DATA_PATH,
+files0 = [os.path.join(tmp_path,
                        '{}{}.{}'.format(prename, chki, basin)) for chki in indices]
 args0 = list(zip(indices,
                  files0))
@@ -105,20 +90,20 @@ with mp.Pool(processes=os.cpu_count()) as mpool:
    _ = all(mpool.starmap(chkvcf.CyvcfVariantChunkGenerator(fingz, chunksize=1000).chunkwriter, args0))
 print('\r\nTime elapsed --> ', time.time() - start)  # 12.9
 
-files0 = os.listdir(prm.TMP_DATA_PATH)
+files0 = os.listdir(tmp_path)
 files1 = ['pooled.{}'.format(f0) for f0 in files0]
 args1 = list(zip(files0,
                  repeat(splits, len(indices)),
                  repeat(df2dict, len(indices)),
                  files1))
-with mp.Pool(processes=os.cpu_count()//2) as mpool:
+with mp.Pool(processes=os.cpu_count()) as mpool:
      _ = all(mpool.starmap(chkvcf.cyvcfchunkhandler_process, args1))
 
 files2 = ['{}.gz'.format(f1) for f1 in files1]
-pybcf.concat(files2, basin, prm.TMP_DATA_PATH)
-pybcf.sort(basin, prm.TMP_DATA_PATH)
-pybcf.bgzip(basin, basout, prm.TMP_DATA_PATH)
-pybcf.index(basout, prm.TMP_DATA_PATH)
+pybcf.concat(files2, basin, tmp_path)
+pybcf.sort(basin, tmp_path)
+pybcf.bgzip(basin, basout, tmp_path)
+pybcf.index(basout, tmp_path)
 delete_file(basin)
 shutil.copy(basout, argsin.pathout)
 print('\r\nTime elapsed --> ', time.time() - start)  # 19.45
